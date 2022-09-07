ded41e8006598592512614668659fa73ff751231
ayxan386 <ayxanpro@gmail.com>
2022-09-07 10:46:51 +0400

    Solved task-2

--- src/main/java/com/nphase/service/ShoppingCartService.java
+++ src/main/java/com/nphase/service/ShoppingCartService.java
@@ -1,17 +1,28 @@
 package com.nphase.service;
 
+import com.nphase.entity.Product;
 import com.nphase.entity.ShoppingCart;
 
-import javax.naming.OperationNotSupportedException;
 import java.math.BigDecimal;
 
 public class ShoppingCartService {
 
+    private static final int DISCOUNT_COUNT = 3;
+    private static final BigDecimal DISCOUNT_FRACTION = BigDecimal.valueOf(0.1);
+
     public BigDecimal calculateTotalPrice(ShoppingCart shoppingCart) {
         return shoppingCart.getProducts()
                 .stream()
-                .map(product -> product.getPricePerUnit().multiply(BigDecimal.valueOf(product.getQuantity())))
+                .map(this::calculatePrice)
                 .reduce(BigDecimal::add)
                 .orElse(BigDecimal.ZERO);
+    }
+
+    private BigDecimal calculatePrice(Product product) {
+        final var res = product.getPricePerUnit().multiply(BigDecimal.valueOf(product.getQuantity()));
+        if (product.getQuantity() > DISCOUNT_COUNT) {
+            return res.multiply(BigDecimal.ONE.subtract(DISCOUNT_FRACTION));
+        }
+        return res;
     }
 }
--- src/test/java/com/nphase/service/ShoppingCartServiceTest.java
+++ src/test/java/com/nphase/service/ShoppingCartServiceTest.java
@@ -10,19 +10,22 @@ import javax.naming.OperationNotSupportedException;
 import java.math.BigDecimal;
 import java.util.Arrays;
 
-public class ShoppingCartServiceTest {
+class ShoppingCartServiceTest {
     private final ShoppingCartService service = new ShoppingCartService();
 
     @Test
-    public void calculatesPrice()  {
+    void calculatesPrice() {
+        BigDecimal expectedResult = BigDecimal.valueOf(40.00);
+
         ShoppingCart cart = new ShoppingCart(Arrays.asList(
-                new Product("Tea", BigDecimal.valueOf(5.0), 2),
-                new Product("Coffee", BigDecimal.valueOf(6.5), 1)
+                new Product("Tea", BigDecimal.valueOf(5.0), 5),
+                new Product("Smoothie", BigDecimal.valueOf(7.0), 1),
+                new Product("Coffee", BigDecimal.valueOf(3.5), 3)
         ));
 
         BigDecimal result = service.calculateTotalPrice(cart);
 
-        Assertions.assertEquals(result, BigDecimal.valueOf(16.5));
+        Assertions.assertEquals(0, expectedResult.compareTo(result));
     }
 
 }
